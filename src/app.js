1431d338224abd773b365ea4ba4df0549bdc29e4
PalomaSalinas19 <palomaiesv@gmail.com>
2024-07-02 11:32:53 +0200

    first commit

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import {pool} from './db.js'
+import {PORT} from './config.js'
+
+const app = express()
+
+app.get('/', async (req, res) => {
+    const [rows] = await pool.query('select * from users');
+    res.json(rows)
+})
+
+app.get('/ping', async (req, res) => {
+    const [result] = await pool.query('select "hello world" as RESULT');
+    res.json(result[0])
+})
+
+app.get('/create', async (req, res) => {
+    const [result] = await pool.query('insert into users(name) values ("John")');
+    res.json(result)
+})
+
+
+app.listen(PORT)
+console.log('Server on port ',PORT)
